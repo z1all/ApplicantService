0ae7b70f97f5eb614ba39b1aa5e4cbb5d04faca7
vitaliy ivanof <ivitalij741@gmail.com>
2024-04-08 14:33:16 +0700

    Add entities

---
+++ DictionaryService/DictionaryService.Core.Domain/EducationDocumentType.cs
@@ -0,0 +1,15 @@
+﻿using Common.Repositories;
+
+namespace DictionaryService.Core.Domain
+{
+    public class EducationDocumentType : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required bool Deprecated { get; set; }
+
+        public required Guid EducationLevelId { get; set; }
+        public EducationLevel? EducationLevel { get; set; }
+
+        public IEnumerable<EducationLevel> NextEducationLevels { get; set; } = null!;
+    }
+}
---
+++ DictionaryService/DictionaryService.Core.Domain/Faculty.cs
@@ -0,0 +1,12 @@
+﻿using Common.Repositories;
+
+namespace DictionaryService.Core.Domain
+{
+    public class Faculty : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required bool Deprecated { get; set; }
+
+        public IEnumerable<EducationProgram> Programs { get; set; } = null!;
+    }
+}
