7cb785473f9018f98cfcbd27efe4c575b0bb394a
vitaliy ivanof <ivitalij741@gmail.com>
2024-04-19 09:27:56 +0700

    Add validation

--- ApplicantService/ApplicantService.Core.Application/DTOs/PassportInfo.cs
+++ ApplicantService/ApplicantService.Core.Application/DTOs/PassportInfo.cs
@@ -1,10 +1,13 @@
-﻿namespace ApplicantService.Core.Application.DTOs
+﻿using ApplicantService.Core.Application.Validation;
+
+namespace ApplicantService.Core.Application.DTOs
 {
     public class PassportInfo
     {
         public required Guid Id { get; set; }
         public required string SeriesNumber { get; set; }
         public required string BirthPlace { get; set; }
+        [DateValidation]
         public required DateOnly IssueYear { get; set; }
         public required string IssuedByWhom { get; set; }
         public required List<ScanInfo> Scans { get; set; }
